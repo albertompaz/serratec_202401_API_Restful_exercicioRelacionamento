a2da1bfe2cebdec6b4e4493f053a09667a78b6f3
LAPTOP-TN11MPJB\alberto.paz <alberto.paz@neki-it.com.br>
2024-05-13 18:22:41 -0300

    feat: criando estrutura do projeto

---
+++ src/main/java/org/serratec/backend/h2banco/H2bancoApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.h2banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2bancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2bancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/backend/h2banco/H2bancoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.backend.h2banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2bancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
